db0f510f3291c35f7036a5655df0cb4a8bc23bda
animanhiu-svg <animanhiu@gmail.com>
2026-03-10 13:20:04 +0400

    Update config.py

--- config.py
+++ config.py
@@ -6,12 +6,15 @@ load_dotenv()
 TG_TOKEN = os.getenv("TG_TOKEN")
 HF_TOKEN = os.getenv("HF_TOKEN")
 
-ALLOWED_USER_ID = 1260479529
+# Только один пользователь
+ALLOWED_USER_ID = 1260479529  # замени на свой
+
 PORT = int(os.getenv("PORT", 10000))
 
 MODEL = "dphn/Dolphin-Mistral-24B-Venice-Edition:featherless-ai"
 BASE_URL = "https://router.huggingface.co/v1"
 
+# Запрещённые темы (оставляем)
 FORBIDDEN_WORDS = [
     "наркотик", "наркота", "героин", "кокаин", "метамфетамин", "спайс", "мефедрон", "амфетамин",
     "детский", "ребёнок", "ребенок", "малолетний", "несовершеннолетний", "педофил", "педофилия",

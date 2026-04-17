6c47d6a03f4a2bb1a6e55e168ce8b1040cdd1986
animanhiu-svg <animanhiu@gmail.com>
2026-04-17 18:03:20 +0400

    Update database.py

--- database.py
+++ database.py
@@ -1,6 +1,9 @@
 user_settings = {}
 user_history = {}
 
+def get_history_key(chat_id, character_id):
+    return f"{chat_id}_{character_id}"
+
 def init_user(chat_id):
     if chat_id not in user_settings:
         user_settings[chat_id] = {
@@ -18,28 +21,31 @@ def update_field(chat_id, field, value):
 def get_field(chat_id, field, default=None):
     return user_settings.get(chat_id, {}).get(field, default)
 
-def add_to_history(chat_id, user_msg, bot_msg):
-    if chat_id not in user_history:
-        user_history[chat_id] = []
+def add_to_history(chat_id, character_id, user_msg, bot_msg):
+    key = get_history_key(chat_id, character_id)
+    if key not in user_history:
+        user_history[key] = []
     if user_msg:
-        user_history[chat_id].append({"role": "user", "content": user_msg})
+        user_history[key].append({"role": "user", "content": user_msg})
     if bot_msg:
-        user_history[chat_id].append({"role": "assistant", "content": bot_msg})
-    # Увеличил до 100 сообщений (50 диалогов)
-    if len(user_history[chat_id]) > 100:
-        user_history[chat_id] = user_history[chat_id][-100:]
+        user_history[key].append({"role": "assistant", "content": bot_msg})
+    if len(user_history[key]) > 100:
+        user_history[key] = user_history[key][-100:]
 
-def get_history(chat_id):
-    # Возвращаем последние 80 сообщений (40 диалогов)
-    return user_history.get(chat_id, [])[-80:]
+def get_history(chat_id, character_id):
+    key = get_history_key(chat_id, character_id)
+    return user_history.get(key, [])[-80:]
 
-def clear_history(chat_id):
-    if chat_id in user_history:
-        user_history[chat_id] = []
+def clear_history(chat_id, character_id):
+    key = get_history_key(chat_id, character_id)
+    if key in user_history:
+        user_history[key] = []
 
 def reset_all(chat_id):
     if chat_id in user_settings:
         del user_settings[chat_id]
-    if chat_id in user_history:
-        del user_history[chat_id]
+    # Удаляем всю историю пользователя
+    keys_to_delete = [k for k in user_history if k.startswith(f"{chat_id}_")]
+    for k in keys_to_delete:
+        del user_history[k]
     init_user(chat_id)

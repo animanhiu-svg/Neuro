d3fe49f2c453c339a7bcaf65e6a2b1117e6b4862
animanhiu-svg <animanhiu@gmail.com>
2026-04-17 15:28:55 +0400

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ HF_TOKEN = os.getenv("HF_TOKEN")
 ALLOWED_USER_ID = 1260479529
 PORT = int(os.getenv("PORT", 10000))
 
-MODEL = "mistralai/Mistral-7B-Instruct-v0.3"
+MODEL = "google/gemma-4-31B-it"
 BASE_URL = "https://router.huggingface.co/v1"
 
 FORBIDDEN_WORDS = []

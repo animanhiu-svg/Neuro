1d3804cbb93de3501309a4f37dce7d04e43fd85f
animanhiu-svg <animanhiu@gmail.com>
2026-04-21 22:51:01 +0400

    Update web_server.py

--- web_server.py
+++ web_server.py
@@ -8,7 +8,7 @@ import config
 from database import init_user, update_field, get_field, get_history, add_to_history, clear_history
 from logic import contains_forbidden, query_dolphin
 
-client = OpenAI(base_url=config.BASE_URL, api_key=config.DEEPSEEK_TOKEN)
+client = OpenAI(base_url=config.BASE_URL, api_key=config.OPENROUTER_TOKEN)
 bot = telebot.TeleBot(config.TG_TOKEN)
 
 app = Flask(__name__, static_folder='mini_app')

bbe25ad65869b7095b87fb647ff7415f31b43abf
animanhiu-svg <animanhiu@gmail.com>
2026-04-22 21:21:42 +0400

    Update config.py

--- config.py
+++ config.py
@@ -4,12 +4,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TG_TOKEN = os.getenv("TG_TOKEN")
-OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
+OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")   # <--- ЭТОТ КЛЮЧ
 
 ALLOWED_USER_ID = 1260479529
-PORT = 8080
+PORT = int(os.getenv("PORT", 8080))
 
-MODEL = "meta-llama/llama-3.1-8b-instruct"
+MODEL = "meta-llama/llama-3.3-70b-instruct"
 BASE_URL = "https://openrouter.ai/api/v1"
 
 FORBIDDEN_WORDS = []

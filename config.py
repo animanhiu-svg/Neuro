7effe8be21c0c7b931c2c8c4a18a2eda415e47e7
animanhiu-svg <animanhiu@gmail.com>
2026-04-23 21:57:19 +0400

    Update config.py

--- config.py
+++ config.py
@@ -4,12 +4,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TG_TOKEN = os.getenv("TG_TOKEN")
-HF_TOKEN = os.getenv("HF_TOKEN")
+HF_TOKEN = "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"
 
 ALLOWED_USER_ID = 1260479529
-PORT = int(os.getenv("PORT", 10000))
+PORT = int(os.getenv("PORT", 8080))
 
-MODEL = "google/gemma-4-31b-it"
+MODEL = "google/gemma-2-9b-it"
 BASE_URL = "https://api-inference.huggingface.co/v1"
 
 FORBIDDEN_WORDS = []

29092a2e7be194307c9bcccdc3c512872080aa99
animanhiu-svg <animanhiu@gmail.com>
2026-04-23 22:36:41 +0400

    Update config.py

--- config.py
+++ config.py
@@ -7,9 +7,9 @@ TG_TOKEN = os.getenv("TG_TOKEN")
 HF_TOKEN = os.getenv("HF_TOKEN")
 
 ALLOWED_USER_ID = 1260479529
-PORT = int(os.getenv("PORT", 10000))
+PORT = int(os.getenv("PORT", 8080))
 
-MODEL = "google/gemma-4-31B-it"
-BASE_URL = "https://router.huggingface.co/v1"
+MODEL = "google/gemma-4-31b-it"
+BASE_URL = "https://api-inference.huggingface.co/v1"
 
 FORBIDDEN_WORDS = []

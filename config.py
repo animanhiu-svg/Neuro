0f53c24fdfec647345ba65ffb60668832b0e6a66
animanhiu-svg <animanhiu@gmail.com>
2026-04-23 20:53:35 +0400

    Update config.py

--- config.py
+++ config.py
@@ -4,12 +4,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TG_TOKEN = os.getenv("TG_TOKEN")
-OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")   # <--- ЭТОТ КЛЮЧ
+HF_TOKEN = os.getenv("HF_TOKEN")
 
 ALLOWED_USER_ID = 1260479529
-PORT = int(os.getenv("PORT", 8080))
+PORT = int(os.getenv("PORT", 10000))
 
-MODEL = "google/gemma-3-27b-it:free"
-BASE_URL = "https://openrouter.ai/api/v1"
+MODEL = "google/gemma-4-31b-it"
+BASE_URL = "https://api-inference.huggingface.co/v1"
 
 FORBIDDEN_WORDS = []

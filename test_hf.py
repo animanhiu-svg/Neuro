80cad7c4cccbf5c44e5902b69680599dd3c2cecb
animanhiu-svg <animanhiu@gmail.com>
2026-04-23 21:56:02 +0400

    Update test_hf.py

--- test_hf.py
+++ test_hf.py
@@ -1,6 +1,20 @@
 import requests
 
-token = "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"  # твой токен
+token = "import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TG_TOKEN = os.getenv("TG_TOKEN")
+HF_TOKEN = "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"
+
+ALLOWED_USER_ID = 1260479529
+PORT = int(os.getenv("PORT", 8080))
+
+MODEL = "google/gemma-2-9b-it"
+BASE_URL = "https://api-inference.huggingface.co/v1"
+
+FORBIDDEN_WORDS = []"  # твой токен
 headers = {"Authorization": f"Bearer {token}"}
 
 # Тест 1: проверка токена
